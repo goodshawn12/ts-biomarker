61acd9626039e4b145ad158eb4610d2fadb20926
goodshawn12 <goodshawn12@gmail.com>
2023-05-18 17:45:53 -0700

    add ecg leads names

--- automatic_ecg_diagnosis/constants.py
+++ automatic_ecg_diagnosis/constants.py
@@ -1,1 +1,19 @@
+from enum import Enum
+
 SAMPLING_RATE_ECG = 400  # Hz
+
+
+# 12-lead ECG channel names
+class ECGLead(Enum):
+    LEAD_I = 'Lead I'
+    LEAD_II = 'Lead II'
+    LEAD_III = 'Lead III'
+    LEAD_aVR = 'Lead aVR'
+    LEAD_aVL = 'Lead aVL'
+    LEAD_aVF = 'Lead aVF'
+    V1 = 'V1'
+    V2 = 'V2'
+    V3 = 'V3'
+    V4 = 'V4'
+    V5 = 'V5'
+    V6 = 'V6'
--- automatic_ecg_diagnosis/plots.py
+++ automatic_ecg_diagnosis/plots.py
@@ -1,8 +1,12 @@
 import numpy as np
 import plotly.graph_objects as go
 
+from automatic_ecg_diagnosis import constants
+
 
 def plot_ecg_tracings(ecg_tracings: np.ndarray = None):
     """Plot ecg tracings from the hdf5 file."""
     if ecg_tracings is None:
         return go.Figure()
+
+    ecg_leads = [lead.value for lead in list(constants.ECGLead)]
