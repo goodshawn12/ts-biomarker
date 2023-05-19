75f2447e8db1865f7d557ea7587926d81e69f93f
goodshawn12 <goodshawn12@gmail.com>
2023-05-18 17:51:38 -0700

    modify the ecg lead names to be consistent with the dataset description

--- automatic_ecg_diagnosis/constants.py
+++ automatic_ecg_diagnosis/constants.py
@@ -5,12 +5,12 @@ SAMPLING_RATE_ECG = 400  # Hz
 
 # 12-lead ECG channel names
 class ECGLead(Enum):
-    LEAD_I = 'Lead I'
-    LEAD_II = 'Lead II'
-    LEAD_III = 'Lead III'
-    LEAD_aVR = 'Lead aVR'
-    LEAD_aVL = 'Lead aVL'
-    LEAD_aVF = 'Lead aVF'
+    DI = 'DI'
+    DII = 'DII'
+    DIII = 'DIII'
+    AVR = 'AVR'
+    AVL = 'AVL'
+    AVF = 'AVF'
     V1 = 'V1'
     V2 = 'V2'
     V3 = 'V3'
